8ace91d5a60ca9c87e4083c9290abe622800cd82
sms-mgrunblatt <mgrunblatt@sms-latam.com>
2020-08-21 09:53:50 -0300

    Basic, react, flux and redux

---
+++ App-react-tutorial/1-basic-react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Layout from './Components/Layout'
+export default function App() {
+  const name ="Will";
+  const name2 ="Emma";
+  
+  return (
+    <Layout/>
+    );
+  }
+
---
+++ App-react-tutorial/1-basic-react/src/Components/Layout.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import Footer from "./Footer"
+import Header from "./Header"
+
+
+class Layout extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			title: "Welcome",
+		};
+	}
+
+	changeTitle(title){
+		this.setState({title});
+	}
+	render(){
+		return(
+			<div>
+			<Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
+			<Footer />
+			</div>
+			);
+	}
+
+}
+
+export default Layout;
